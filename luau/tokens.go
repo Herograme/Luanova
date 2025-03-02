417693f45d81b99115294da541da4c65145e00a3
Murillo Deolino <bye456oi@gmail.com>
2025-03-02 01:10:18 -0300

    "init"

--- luau/tokens.go
+++ luau/tokens.go
@@ -2,98 +2,76 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package syntax
-
-type Token uint
-
-type token = Token
+package luau
 
 const (
-	_ token = iota
+	Illegal = iota
+	EOF
 
-	// Special tokens
-	_EOF     // EOF
-	_Name    // name
-	_Number  // number
-	_String  // string
-	_Comment // comment
-
-	// Operators
-	_Add    // +
-	_Sub    // -
-	_Mul    // *
-	_Div    // /
-	_Mod    // %
-	_Pow    // ^
-	_Concat // ..
-	_Len    // #
-	_Eq     // ==
-	_Ne     // ~=
-	_Le     // <=
-	_Ge     // >=
-	_Lt     // <
-	_Gt     // >
-	_And    // and
-	_Or     // or
-	_Not    // not
-
-	// Assignment operators
-	_Assign       // =
-	_AddAssign    // +=
-	_SubAssign    // -=
-	_MulAssign    // *=
-	_DivAssign    // /=
-	_ModAssign    // %=
-	_PowAssign    // ^=
-	_ConcatAssign // ..=
-
-	// Delimiters
-	_Lparen   // (
-	_Rparen   // )
-	_Lbrace   // {
-	_Rbrace   // }
-	_Lbrack   // [
-	_Rbrack   // ]
-	_Comma    // ,
-	_Semi     // ;
-	_Colon    // :
-	_Dot      // .
-	_Dots     // ...
-	_Arrow    // ->
-	_Question // ?
+	Comment // //
+	CommentBlock // /**/
+	
+	Literal // Literal
 
 	// Keywords
-	_And
-	_Break
-	_Continue
-	_Do
-	_Else
-	_Elseif
-	_End
-	_False
-	_For
-	_Function
-	_If
-	_In
-	_Local
-	_Nil
-	_Not
-	_Or
-	_Repeat
-	_Return
-	_Then
-	_True
-	_Until
-	_While
+	Function // Function
+	Local    // Local
+	If       // If
+	ElseIf   // ElseIf
+	Else     // Else
+	While    // While
+	For      // For
+	End      // End
+	Then     // Then
+	Repeat   // Repeat
+	Continue // Continue
+	Break    // Break
+	In       // In
+	Return   // Return
+	Do       // Do
 
-	// Luau-specific keywords
-	_Type
-	_Export
-	_Enum
-	_Interface
-	_As
-	_Is
-	_Where
+	// Operators
+	Not          // Not
+	NotEqual     // ~=
+	Or           // Or
+	Greater      // <
+	Less         // >
+	GreaterEqual // <=
+	LessEqual    // >=
+	And          // And
+	Assign       // =
+	Equal        // ==
+	PlusAssign   // +=
+	SubAssign    // -=
+	MultiAssign  // *=
+	DivAssign    // /=
+	ModAssign    // %=
+	Declaration  // =
+	Plus         // +
+	Sub          // -
+	Multi        // *
+	Div          // /
+	Mod          // %
+	Po           // ^
+	Concat       // ..
 
-	tokenCount
+	//Delimiters
+	LParen   // (
+	RParen   // )
+	LBrace   // {
+	RBrace   // }
+	LBrack   // [
+	RBrack   // ]
+	Comma    // ,
+	Semi     // ;
+	Colom    // :
+	Dot      // .
+	Dots     // ...
+	Arrow    // ->
+	Question // ?
+
+	// Primitive Types
+	String // string
+	Int    // int
+	Float  // float
 )
