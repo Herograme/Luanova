a8ab60c51e1c1a7e6152382e2b3f4696e3eebd08
Samuel Borges <Herograme@gmail.com>
2025-03-02 02:04:44 -0300

    Add VSCode extension(not published) for Lua Nova language support
    This commit adds a VSCode extension for Lua Nova, including:
    - Syntax highlighting configuration
    - Language configuration
    - Basic extension metadata
    - Initial project structure for VSCode extension

--- luanova/tokens.go
+++ luanova/tokens.go
@@ -2,15 +2,15 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package luau
+package luanova
 
 const (
 	Illegal = iota
 	EOF
 
-	Comment // //
+	Comment      // //
 	CommentBlock // /**/
-	
+
 	Literal // Literal
 
 	// Keywords
